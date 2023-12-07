36afb50a73f4ac31a1c7310d028656c1aaf23de1
Turbobot <turbobot@vercel.com>
2023-12-07 18:58:54 +0000

    feat(create-turbo): apply official-starter transform

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const App = (): JSX.Element => (
-  <div>
-    <h1>thom app</h1>
-  </div>
-);
-
-export { App };
--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-import { App } from './App';
-
-const container = document.getElementById('thom-app') as HTMLElement;
-const root = createRoot(container);
-root.render(<App />);
